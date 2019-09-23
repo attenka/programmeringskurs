ce8774472e87282edc74bf8efa9451eb60f19961
Atte Tenkanen <attetenkanen@Attes-MacBook-Air.local>
2019-09-23 19:58:32 +0300

    Började programmeringskursen

---
+++ quiz.py
@@ -0,0 +1,24 @@
+
+
+def check_answer(guess, answer):
+    global points
+    if guess.lower() == answer.lower():
+        points += 1
+        print("Rätt!")
+    else:
+        print("Inter riktigt...")
+
+points=0
+print("Dags för quiz!")
+print("--------------")
+
+guess1 = input("Är Niinistö Finlands president?              ")
+check_answer(guess1, "ja")
+guess1= input("Vilken var Finlands första huvudstad?")
+check_answer(guess1, "Åbo")
+guess1= input("I vilken stad ligger museet Kiasma?")
+check_answer(guess1, "Helsingfors")
+
+print("---------------------")
+print("Du fick " + str(points) + " poöng!")
+
